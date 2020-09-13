de7869fedeb20bfae149c6a15ff0358d3fa08e16
알파카 <rhehd721@naver.com>
2020-09-13 18:25:16 +0900

    3주차

--- 첫번째 실습/firstrest/firstrest/urls.py
+++ 첫번째 실습/firstrest/firstrest/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 import app.urls
 
+# from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+# router.register('post', views.PostVireSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
